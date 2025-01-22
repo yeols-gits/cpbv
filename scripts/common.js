67b6315e843962acbeeacf8dab53679debe28440
khy <celinelove23@naver.com>
2025-01-22 13:20:01 +0900

    common.js delete

--- scripts/common.js
+++
@@ -1,4 +0,0 @@
-const setSearchData = (key, data) => { window.commonData.setSearchData(key, data); }
-const getSearchData = (key) => { return window.commonData.getSearchData(key); }
-const setPlayer = (data) => { window.commonData.setPlayer(data); }
-const getSelectedPlayer = () => { return window.commonData.getSelectedPlayer(); }
